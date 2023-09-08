cf6c208637d889c6adbc0002dd3e96691649db06
tandavbot2 <142794511+tandavbot2@users.noreply.github.com>
2023-09-08 22:21:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -19,4 +19,6 @@ class Config:
     BOT_THUMB = f"{os.path.dirname(__file__)}/bot_thumb.jpg"
     MAX_CONCURRENT_TASKS = 50
     MAX_TASK_DURATION_EXTRACT = 45 * 60  # 45 minutes (in seconds)
-    MAX_TASK_DURATION_MERGE = 90 * 60  # 1 hour 30 minutes (in seconds)
+    #MAX_TASK_DURATION_MERGE = 24 * 60 * 60  # 1 hour 30 minutes (in seconds)
+    MAX_TASK_DURATION_MERGE = 24 * 60 * 60  # 24 hours in seconds
+    #MAX_TASK_DURATION_MERGE = 90 * 60  # 1 hour 30 minutes (in seconds)
